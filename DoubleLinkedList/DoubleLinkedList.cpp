812b178ce58570f3a375bbd2e974f90a2796e657
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-05 10:54:26 +0700

    jika data yang dimasukkan tidak ada dalam sistem maka akan muncul tulisan seperti tertera

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -192,3 +192,11 @@ void searchData()
 		cout << "\nList is empty" << endl;
 
 	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whise record you want to search: ";
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+
